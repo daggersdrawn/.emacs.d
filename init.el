9d310f37fb35abb4a383c9599a581265a19197c6
Thomas Schreiber <schreibert@email.chop.edu>
2018-02-01 12:45:24 -0500

    Configure use-package.

--- init.el
+++ init.el
@@ -5,5 +5,14 @@
 ;; and brighter; it simply makes everything else vanish."
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 
+;; MELPA (Milkypostman’s Emacs Lisp Package Archive)
+;; The largest and most up-to-date repository of Emacs packages.
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; Isolate package configuration in a performance-oriented and tidy way
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
