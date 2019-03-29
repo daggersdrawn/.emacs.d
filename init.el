8a9b8099ca96420edccd2078e543cd48db1caf8f
Thomas Schreiber <schreibert@email.chop.edu>
2019-03-29 16:33:32 -0400

    Web Mode is an autonomous emacs major-mode for editing web templates.

--- init.el
+++ init.el
@@ -320,3 +320,17 @@
   (use-package git-gutter
     :ensure t
     :init (global-git-gutter-mode)))
+
+;; Web Mode is an autonomous emacs major-mode for editing web templates.
+;;   https://web-mode.org
+(use-package web-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mdx?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2))
