4a546382fcedaf51a56d8829b36bd457778a43a9
Thomas Schreiber <rizumu@users.noreply.github.com>
2024-02-05 04:09:21 -0500

    Install tree-sitter language grammars without prompt.

--- init.el
+++ init.el
@@ -399,7 +399,7 @@
 
 (use-package treesit-auto
   :custom
-  (treesit-auto-install 'prompt)
+  (treesit-auto-install t)
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
